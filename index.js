2fec8b3d873369e7577b3e05094ecad4a8b53542
Keelana Char <keelanachar@gmail.com>
2019-12-09 20:38:06 -0600

    add vehicle to vehicles array

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var app = new Vue({
   el: "#app",
   data: {
     message: "Hello Vue!",
-    showInfo: false
+    showInfo: false,
+    vehicles: ["tacoma", "civic", "supra"],
+    newVehicle: ""
   },
   methods: {
     changeMessage: function() {
@@ -16,6 +18,11 @@ var app = new Vue({
       } else {
         this.showInfo = true;
       }
+    },
+    addVehicle: function() {
+      console.log("adding a vehicle");
+      console.log(this.newVehicle);
+      this.vehicles.push(this.newVehicle);
     }
   }
 });
