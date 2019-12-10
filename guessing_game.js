409fbacfe671ecff82ec4c3ebd195992491a762a
Keelana Char <keelanachar@gmail.com>
2019-12-09 21:49:10 -0600

    change userNumber variable name, realized parseInt isnt needed?

--- guessing_game.js
+++ guessing_game.js
@@ -3,14 +3,14 @@ var app = new Vue({
   data: {
     message: "Hello Vue!",
     myNumber: 45,
-    number: 0,
+    userNumber: 0,
     result: ""
   },
   methods: {
     usersGuess: function() {
-      if (parseInt(this.number) === parseInt(this.myNumber)) {
+      if (this.userNumber === this.myNumber) {
         this.result = "correct!";
-      } else if (parseInt(this.number) < parseInt(this.myNumber)) {
+      } else if (this.userNumber < this.myNumber) {
         this.result = "too low";
       } else {
         this.result = "too high";
