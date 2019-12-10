7253e62631cdd86c4b256878a455102e866dceef
Keelana Char <keelanachar@gmail.com>
2019-12-09 20:21:48 -0600

    use v-if to toggle

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var app = new Vue({
   el: "#app",
   data: {
-    message: "Hello Vue!"
+    message: "Hello Vue!",
+    showInfo: false
   },
   methods: {
     changeMessage: function() {
       console.log("..changing..");
       this.message = "changed";
+    },
+    toggleInfo: function() {
+      console.log("message toggling");
+      if (this.showInfo === true) {
+        this.showInfo = false;
+      } else {
+        this.showInfo = true;
+      }
     }
   }
 });
